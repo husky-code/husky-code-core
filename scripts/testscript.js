1f1c5095bc6ebd72bafb7e2b6132a61e72bdd19a
E70M <eitanmyron@outlook.com>
2019-04-15 17:01:07 -0700

    Basic file organization
    Extra features for test setup, husky logo links to our GitHub repo

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -2,11 +2,13 @@ function load() {
 	var http = new XMLHttpRequest();
 	var url = "https://httpbin.org/get";
 	http.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			var json = JSON.parse(this.responseText);
-			document.getElementById("anchor").innerHTML = "Origin: " + json.origin + "\nURL: " + json.url + "\nDnt: " + json.headers.Dnt;
-		} else {
-			document.getElementById("anchor").innerHTML = "Could not connect to server.";
+		if(this.readyState == 4) {
+			if(this.status == 200) {
+				var json = JSON.parse(this.responseText);
+				document.getElementById("anchor").innerHTML = "Origin: " + json.origin + "\nURL: " + json.url + "\nDnt: " + json.headers.Dnt;
+			} else {
+				document.getElementById("anchor").innerHTML = "Could not display from server.";
+			}
 		}
 	};
 	http.open("GET", url, true);
