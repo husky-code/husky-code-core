fb65cb0e9a0e7c14acae1a86e3b7051768e9fdef
E70M <eitanmyron@outlook.com>
2019-04-24 16:10:15 -0700

    Bootstrap
    Grid system, added delta counter to JS

--- scripts/testscript.js
+++ scripts/testscript.js
@@ -1,13 +1,17 @@
+var delta = 0;
 function load() {
 	var http = new XMLHttpRequest();
 	var url = "https://httpbin.org/get";
 	http.onreadystatechange = function() {
 		if(this.readyState == 4) {
 			if(this.status == 200) {
+				delta++;
 				var json = JSON.parse(this.responseText);
-				document.getElementById("anchor").innerHTML = "Origin: " + json.origin + "\nURL: " + json.url + "\nDnt: " + json.headers.Dnt;
+				document.getElementById("anchor").innerHTML = "Origin: " + json.origin + "\nURL: " + json.url + "\nDnt: " + json.headers.Dnt 
+					+ "\nDelta: " + delta;
 			} else {
 				document.getElementById("anchor").innerHTML = "Could not display from server.";
+				delta = 0;
 			}
 		}
 	};
