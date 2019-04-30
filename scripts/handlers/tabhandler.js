a811b40378fac8b946c398e780e9ebd84d403122
E70M <eitanmyron@outlook.com>
2019-04-29 21:19:37 -0700

    Tab handler

--- scripts/handlers/tabhandler.js
+++ scripts/handlers/tabhandler.js
@@ -1,3 +1,13 @@
 function switchTab(tab) {
 	// parse tab id, switch to that tab's content
+	var frame = document.querySelector("#window");
+	if (tab == 'mycode') {
+    	frame.src = "/mycode.html";
+  	} else if (tab == 'discussion') {
+    	frame.src = "/discussions.html";
+  	} else if (tab == 'solutions') {
+    	frame.src = "/solutions.html";
+	} else if (tab == 'timer') {
+    	frame.src = "timer.html";
+  	}
 }
