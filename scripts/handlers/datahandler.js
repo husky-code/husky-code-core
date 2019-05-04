16164afd4e7412dae80ea8360d0d4a89446427ab
E70M <eitanmyron@outlook.com>
2019-05-04 14:57:25 -0700

    Data handler
    TODO: simplify regex parser, write test cases file parser, correct IDs

---
+++ scripts/handlers/datahandler.js
@@ -0,0 +1,38 @@
+function handleData(url, args, func, requestType) {
+	var xhr = new XMLHttpRequest();
+	var params = "?";
+	args = JSON.stringify(args);
+	// TODO: params += args.replace(/(")(,)(:)( )({)(})/g, "[replace this with some regex?]");
+	params += args.replace(/"/g, "");
+	params = params.replace(/,/g, "&");
+	params = params.replace(/:/g, "=");
+	params = params.replace(/ /g, "+");
+	params = params.replace(/{/g, "");
+	params = params.replace(/}/g, "");
+	xhr.onreadystatechange = function() {
+		if (this.readystate == 4) {
+			if (this.status == 200) {
+				func(this);
+			} else {
+				func(null);
+			}
+		}
+	};
+	xhr.open(requestType, url + params, true);
+	xhr.send();
+}
+
+function getQuestion(xhr) {
+	// TODO: replace temp IDs with correct ones
+	if (xhr != null) {
+		var data = JSON.parse(xhr.responseText);
+		document.querySelector("#name").innerHTML = data.name;
+		document.querySelector("#author").innerHTML = data.author;
+		document.querySelector("#description").innerHTML = data.description;
+		document.querySelector("#tags").innerHTML = data.tags; // might require separate parsing
+		document.querySelector("#difficulty").innerHTML = data.difficulty;
+		// TODO: write test cases file parser
+	} else {
+		document.querySelector("#description").innerHTML = "Could not load question, please try again later.";
+	}
+}
