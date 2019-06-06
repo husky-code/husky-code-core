856196d60254eba75d31a08bb83adca0d5f86102
E70M <eitanmyron@outlook.com>
2019-06-05 19:21:27 -0700

    Routing, scenes

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Link, Route, Switch } from 'react-router-dom';
 import Question from './scenes/Question';
 import Discussion from './scenes/Discussion';
+import InterviewPrep from './scenes/InterviewPrep';
+import Dashboard from './scenes/Dashboard';
 import './index.css';
 
 class App extends React.Component {
@@ -17,8 +19,8 @@ class App extends React.Component {
         	<div className="layout">
             	<Route exact={true} path="/" component={Question}/>
             	<Route path="/discussion" component={Discussion}/>
-            	<Route path="/interviewprep" component={Discussion}/>
-            	<Route path="/dashboard" component={Discussion}/>
+            	<Route path="/interviewprep" component={InterviewPrep}/>
+            	<Route path="/dashboard" component={Dashboard}/>
         	</div>
     	);
 	}
---
+++ scenes/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/NavBar';
+
+class Dashboard extends React.Component {
+	render() {
+		return (
+			<div className="select-bar-container">
+				<NavBar/>
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
