831f40e23e386c2dba6215a290bb19293f472d66
E70M <eitanmyron@outlook.com>
2019-06-23 15:08:06 -0700

    Server

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var router = express.Router();
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+router.get('/', (req, res) => {
+	//res.send('<h1>Hello, world!</h1>' + req.data);
+	res.render('Question');
 });
+
+router.post('/redirect', (req, res) => {
+	res.send({ data: req.body.data });
+});
+
+app.use('/', router);
 
 app.listen(PORT, () => {
 	console.log(`App is up and running. Listening on port ${PORT}`);
--- services/compiler/wrapper.js
+++ services/compiler/wrapper.js
@@ -6,6 +6,7 @@ module.exports = {
 	wrapScript: function(script, lang) {
 		if (lang == "java") {
 			return javaWrapper.header + script + javaWrapper.footer;
+			//return `public class Test { public static void main(String[] args) { ${this.script} } }`;
 		}
 		return "";
 	}
