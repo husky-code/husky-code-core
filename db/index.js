4f2558d93b8e7ffe3b6f16dd0920c59e8e4ddd2a
E70M <eitanmyron@outlook.com>
2019-07-03 12:44:43 -0700

    DB, server

--- db/index.js
+++ db/index.js
@@ -1,44 +1,41 @@
 var Connection = require('tedious').Connection,
-	Request = require('tedious').Request;
+	Request = require('tedious').Request,
+	TYPES = require('tedious').TYPES;
 const config = require('./config');
 
-// Create connection to database
-var connection = new Connection(config.sqlconfig);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected to Azure SQL Server");
-    }
-});
-
 module.exports = {
-	queryDatabase: (query) => {
-		var data = [];
-    	console.log('Reading rows from the Table...');
-    	// Read all rows from table
-    	var request = new Request(query, (err, rowCount, rows) => {
-            console.log(rowCount + ' row(s) returned');
-            //process.exit();
-        });
-    	request.on('row', (columns) => {
-        	var rowData = [];
-        	columns.forEach((column) => {
-            	//console.log("%s\t%s", column.metadata.colName, column.value);
-            	rowData = column.value;
-        	});
-        	//module.exports.data.push(rowData);
-        	data.push(rowData);
-    	});
-    	request.on('doneInProc', (rowCount, more, rows) => {
-    		//console.log(rows);
-    		// console.log(module.exports.data);
-    		console.log(data);
-    		//return data;
-    	});
-    	connection.execSql(request);
-	}// ,
-// 	data: []
+	// queryDatabase: (query) => {
+//     	console.log('Reading rows from the Table...');
+//     	// Read all rows from table
+//     	var request = new Request(query, (err, rowCount, rows) => {
+//             console.log(rowCount + ' row(s) returned');
+//             //process.exit();
+//         });
+//     	request.on('row', (columns) => {
+//         	columns.forEach((column) => {
+//             	//console.log("%s\t%s", column.metadata.colName, column.value);
+//             	//console.log(column.value);
+//         	});
+//     	});
+//     	request.on('doneInProc', (rowCount, more, rows) => {
+//     		//console.log(data);
+//     		rows.forEach((row, i) => {
+//     			console.log(row[i].value);
+//     		});
+//     	});
+//     	connection.execSql(request);
+// 	}
+	init: () => {
+		// Create connection to database
+		var connection = new Connection(config.sqlconfig);
+		// Attempt to connect and execute queries if connection goes through
+		connection.on('connect', (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Connected to Azure SQL Server");
+			}
+		});
+		return connection;
+	}
 };
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express'),
 	router = express.Router(),
 	logger = require('morgan'),
 	bodyParser = require('body-parser');
+var Connection = require('tedious').Connection,
+	Request = require('tedious').Request,
+	TYPES = require('tedious').TYPES;
 const PORT = process.env.PORT || 3000;
 const db = require('./db');
+
+var connection = db.init();
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +21,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/users', (req, res) => {
-	res.send('<h1>Users</h1>' + db.queryDatabase("SELECT * FROM USERS WHERE NETID='jonsnow' FOR JSON PATH"));
+	console.log('Reading rows from the Table...');
+    // Read all rows from table
+    var request = new Request("SELECT * FROM USERS FOR JSON PATH", (err, rowCount, rows) => {
+    	console.log(rowCount + ' row(s) returned');
+        //process.exit();
+    });
+    request.on('doneInProc', (rowCount, more, rows) => {
+    	//console.log(data);
+    	rows.forEach((row, i) => {
+    		console.log(row[i].value);
+    	});
+		res.send(rows);
+    });
+    connection.execSql(request);
+	//res.send('<h1>Users</h1>' + db.queryDatabase("SELECT * FROM USERS FOR JSON PATH")); //WHERE NETID='jonsnow'
 });
 
 router.post('/createUser', (req, res) => {
