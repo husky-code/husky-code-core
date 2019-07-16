f7167cba626f706a4124d97d9a96bc70a6007ec6
E70M <eitanmyron@outlook.com>
2019-07-16 02:19:08 -0700

    Routes
    Added hashing, must include initialized data model

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	bcrypt = require('bcrypt');
 
-const db = require('../db');
+const db = require('../db'),
+	BCRYPT_SALT_ROUNDS = 12;
+	
+// TODO: const User = require('../models/user') // or something
 
 router.get('/', (req, res) => {
 	res.send('<h1>Hello, world!</h1>');
@@ -27,22 +31,58 @@ router.get('/user/:netid', (req, res) => {
 
 // POST new user
 router.post('/register', (req, res) => {
-	db.query('user', req, res);
+	//db.query('user', req, res);
+	const data = {
+		netid: req.body.netid,
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		class: req.body.class,
+		passwd: req.body.passwd
+	};
+	if (netid === '' || firstname === '' || lastname === '' || passwd === '') {
+		res.json("Missing required information");
+	}
+	User.findOne({
+		where: {
+			netid: data.netid
+		}	
+	}).then(user => {
+		if (user != null) {
+			console.log("user already exists");
+			res.json("user already exists");
+		} else {
+			bcrypt.hash(data.passwd, BCRYPT_SALT_ROUNDS).then((hashedPasswd) => {
+				console.log(hashedPasswd);
+				User.create({
+					netid: data.netid,
+					firstname: data.firstname,
+					lastname: data.lastname,
+					class: data.class,
+					passwd: hashedPasswd
+				});
+			}).then(() => {
+				console.log('user created');
+				res.json('user created');
+			});
+		}
+	}).catch(err => {
+		res.status(500).json(err);
+	});
 });
 
 // PUT updated user information, must update all fields
 router.put('/user:netid', (req, res) => {
-	db.query('user', req, res);
+	//db.query('user', req, res);
 });
 
 // PATCH updated user information, does not need to update all fields
 router.patch('/user/:netid', (req, res) => {
-	db.query('user', req, res);
+	//db.query('user', req, res);
 });
 
 // DELETE existing user
 router.delete('/user/:netid', (req, res) => {
-	db.query('user', req, res);
+	//db.query('user', req, res);
 });
 
 // Handle HTTP Error 404 (page not found)
