607a4c90605260800f919d87fe4f4872d4c81a10
E70M <eitanmyron@outlook.com>
2019-07-18 01:08:32 -0700

    DB, routes
    Routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	router = express.Router(),
 	bcrypt = require('bcrypt');
 
+const users = require('./users');
+
 const db = require('../db'),
 	BCRYPT_SALT_ROUNDS = 12,
 	Users = db.User;
@@ -10,35 +12,10 @@ router.get('/', (req, res) => {
 	res.send('<h1>Hello, world!</h1>');
 });
 
-/* User controllers */
+/* Models routes */
+router.use('/users', users);
 
-// GET all users
-router.get('/users', (req, res) => { // For dev purposes only
-	Users.findAll().then(users => {
-		if (users == null) {
-			res.json('no existing users');
-		} else {
-			res.send(users);
-		}
-	}).catch(err => {
-		res.status(500).json(err);
-	});
-});
-
-// GET user by id
-router.get('/user/:netid', (req, res) => {
-	Users.findByPk(req.params.netid).then(user => {
-		if (user == null) {
-			res.json('user does not exist');
-		} else {
-			res.send(user);
-		}
-	}).catch(err => {
-		res.status(500).json(err);
-	});
-});
-
-// POST new user
+// Register new user
 router.post('/register', (req, res) => {
 	const data = {
 		netid: req.body.netid,
@@ -72,63 +49,6 @@ router.post('/register', (req, res) => {
 			}).then(() => {
 				console.log('user created');
 				res.json('user created');
-			});
-		}
-	}).catch(err => {
-		res.status(500).json(err);
-	});
-});
-
-// PUT updated user information, must update all fields
-router.put('/user:netid', (req, res) => {
-	//db.query('user', req, res);
-});
-
-// PATCH updated user information, does not need to update all fields
-router.patch('/user/:netid', (req, res) => {
-	Users.findOne({
-		where: {
-			netid: req.params.netid
-		}
-	}).then(user => {
-		if (user == null) {
-			console.log('user does not exist');
-			res.json('user does not exist');
-		} else {
-			var json = "";
-			Object.keys(req.body).forEach((key, i) => {
-				json += (i > 0 ? ',' : '') + '"' + key + '":"' + req.body[key] + '"';
-			});
-			Users.update(JSON.parse('{' + json + '}'), {
-				where: {
-					netid: req.params.netid
-				}
-			}).then(() => {
-				res.send('Query succeeded');
-			});
-		}
-	}).catch(err => {
-		res.status(500).json(err);
-	});
-});
-
-// DELETE existing user
-router.delete('/user/:netid', (req, res) => {
-	Users.findOne({
-		where: {
-			netid: req.params.netid
-		}
-	}).then(user => {
-		if (user == null) {
-			console.log('user does not exist');
-			res.json('user does not exist');
-		} else {
-			Users.destroy({
-				where: {
-					netid: req.params.netid
-				}
-			}).then(() => {
-				res.send('Query succeeded');
 			});
 		}
 	}).catch(err => {
---
+++ routes/users/index.js
@@ -0,0 +1,90 @@
+var express = require('express'),
+	router = express.Router();
+
+const db = require('../../db'),
+	Users = db.User;
+	
+// GET all users
+router.get('/', (req, res) => { // For dev purposes only
+	Users.findAll().then(users => {
+		if (users == null) {
+			res.json('no existing users');
+		} else {
+			res.send(users);
+		}
+	}).catch(err => {
+		res.status(500).json(err);
+	});
+});
+
+// GET user by id
+router.get('/:netid', (req, res) => {
+	Users.findByPk(req.params.netid).then(user => {
+		if (user == null) {
+			res.json('user does not exist');
+		} else {
+			res.send(user);
+		}
+	}).catch(err => {
+		res.status(500).json(err);
+	});
+});
+
+// PUT updated user information, must update all fields
+router.put('/:netid', (req, res) => {
+	//db.query('user', req, res);
+});
+
+// PATCH updated user information, does not need to update all fields
+router.patch('/:netid', (req, res) => {
+	Users.findOne({
+		where: {
+			netid: req.params.netid
+		}
+	}).then(user => {
+		if (user == null) {
+			console.log('user does not exist');
+			res.json('user does not exist');
+		} else {
+			var json = "";
+			Object.keys(req.body).forEach((key, i) => {
+				json += (i > 0 ? ',' : '') + '"' + key + '":"' + req.body[key] + '"';
+			});
+			Users.update(JSON.parse('{' + json + '}'), {
+				where: {
+					netid: req.params.netid
+				}
+			}).then(() => {
+				res.send('Query succeeded');
+			});
+		}
+	}).catch(err => {
+		res.status(500).json(err);
+	});
+});
+
+// DELETE existing user
+router.delete('/:netid', (req, res) => {
+	Users.findOne({
+		where: {
+			netid: req.params.netid
+		}
+	}).then(user => {
+		if (user == null) {
+			console.log('user does not exist');
+			res.json('user does not exist');
+		} else {
+			Users.destroy({
+				where: {
+					netid: req.params.netid
+				}
+			}).then(() => {
+				res.send('Query succeeded');
+			});
+		}
+	}).catch(err => {
+		res.status(500).json(err);
+	});
+});
+
+module.exports = router;
